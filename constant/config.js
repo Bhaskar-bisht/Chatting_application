af61168af3f464d761e25b687c11fd66828729a0
bhaskar_bisht <bisht3182568@gmail.com>
2024-06-05 23:17:08 +0530

    change origin

--- constant/config.js
+++ constant/config.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     origin: [
-      "http://localhost:5173",
+      "https://talkhub-y5nb.onrender.com",
       "http://localhost:4173",
       process.env.CLIENT_URL,
     ],
