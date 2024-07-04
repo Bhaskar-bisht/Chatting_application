0a0d897f12cb94d77bea4c37bce6c727dbc57a3f
Bhaskar-bisht <139526056+Bhaskar-bisht@users.noreply.github.com>
2024-07-04 10:40:05 +0530

    Update config.js

--- constant/config.js
+++ constant/config.js
@@ -1,8 +1,6 @@
 const corsOptions = {
     origin: [
       "https://chatweb-application-cogy.onrender.com",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
